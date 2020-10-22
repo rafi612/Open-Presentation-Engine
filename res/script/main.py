67cb4d513a79984cc28a5d823497db927467e996
rafi612 <rafi612lol@gmail.com>
2020-10-22 20:40:28 +0200

    Resources

--- res/script/main.py
+++ res/script/main.py
@@ -1,8 +1,11 @@
 import ope.slide as slide
 
+# set fullscreen mode
 slide.setFullscreen(True)
 
 # here write code
+# to create slide use slide.createSlide(<number>,"path to file") method
+# or use autoscript
 
 
 #end
