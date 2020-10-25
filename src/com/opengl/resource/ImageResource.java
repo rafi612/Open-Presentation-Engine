983549fdeeded4f2ef9b42fccb4eb28d92f516a7
rafi612 <rafi612lol@gmail.com>
2020-10-25 14:24:36 +0100

    Split image creation into 2 constructors

--- src/com/opengl/resource/ImageResource.java
+++ src/com/opengl/resource/ImageResource.java
@@ -3,14 +3,17 @@ package com.opengl.resource;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
+import com.io.Stream;
 import com.jogamp.opengl.util.texture.Texture;
 import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
+import com.main.Main;
 import com.opengl.main.Presentation;
 
 public class ImageResource {
@@ -22,20 +25,21 @@ public class ImageResource {
 	public BufferedImage image = null;
 	
 	@SuppressWarnings("deprecation")
-	public ImageResource (String path) 
+	public ImageResource(String path) 
 	{
 		URL url = null;
 
-		if (path.charAt(0) == '/') url = ImageResource.class.getResource(path);
-		else
-			try 
-		    {
-				url = new File(path).toURL();
-			} 
-		    catch (MalformedURLException e1) 
-		    {
-				e1.printStackTrace();
-			}
+//		if (path.charAt(0) == '/') 
+//			url = ImageResource.class.getResource(path);
+//		else
+		try 
+		{
+			url = new File(path).toURL();
+		} 
+		catch (MalformedURLException e1) 
+		{
+			e1.printStackTrace();
+		}
 		
 		try 
 		{
@@ -46,6 +50,25 @@ public class ImageResource {
 		{
 			//e.printStackTrace();
 			JOptionPane.showMessageDialog(null, "File " + path + " not found");
+		}
+		
+		if (image != null)
+		{
+			image.flush();
+		}
+	}
+	
+	public ImageResource(InputStream i) 
+	{		
+		try 
+		{
+			//System.out.println(url);
+			image = ImageIO.read(i);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "File in jar not found");
 		}
 		
 		if (image != null)
