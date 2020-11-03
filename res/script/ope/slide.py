fb84ea4df106e66852ad540616c52f8bc2375fcc
rafi612 <rafi612lol@gmail.com>
2020-11-03 12:06:33 +0100

    Added menu icons

--- res/script/ope/slide.py
+++ res/script/ope/slide.py
@@ -5,10 +5,13 @@ slides = 0;
 
 fullscreen = True
 music = "null"
+ttskey = "auto"
 
 slidespath = []
 slidesbg = []
 slidestts = []
+slidesentani  = []
+slidesexiani  = []
 
 def saveFileWriteLine(name,text):
     f = open(name,"a+");
@@ -26,6 +29,8 @@ def createSlide(num,path):
     slidespath.append(path)
     slidesbg.append("null")
     slidestts.append("null")
+    slidesentani.append("null")
+    slidesexiani.append("null")
 
 def setSlideBg(num,path):
     global slidesbg;
@@ -36,6 +41,16 @@ def setSlideTTS(num,path):
     global slidestts;
     del slidestts[num - 1]
     slidestts.insert(num - 1,path)
+    
+def setEntranceAnimation(num,path):
+    global slidesentani;
+    del slidesentani[num - 1]
+    slidesentani.insert(num - 1,path)
+
+def setExitAnimation(num,path):
+    global slidesexiani;
+    del slidesexiani[num - 1]
+    slidesexiani.insert(num - 1,path)
 
 def setFullscreen(arg0):
     global fullscreen
@@ -44,6 +59,10 @@ def setFullscreen(arg0):
 def setGeneralMusic(m):
     global music
     music = m
+    
+def setTTSKey(k):
+    global ttskey
+    ttskey = k
   
 def End():
     f = open("config.xml","w+")
@@ -55,12 +74,14 @@ def End():
         saveFileWriteLine("config.xml","    <path>" + slidespath[i] + "</path> \n");
         saveFileWriteLine("config.xml","    <bg>" + slidesbg[i] + "</bg> \n")
         saveFileWriteLine("config.xml","    <tts>" + slidestts[i] + "</tts> \n")
+        saveFileWriteLine("config.xml","    <ent_ani>" + slidesentani[i] + "</ent_ani> \n")
+        saveFileWriteLine("config.xml","    <exi_ani>" + slidesexiani[i] + "</exi_ani> \n")
         saveFileWriteLine("config.xml","</slide" + str(i + 1) + "> \n")
     saveFileWriteLine("config.xml","<summary> \n");
     saveFileWriteLine("config.xml","    <slides>" + str(slides) + "</slides> \n")
     saveFileWriteLine("config.xml","    <fullscreen>" + str(fullscreen) + "</fullscreen> \n")
     saveFileWriteLine("config.xml","    <general_music>" + music + "</general_music> \n")
-    saveFileWriteLine("config.xml","    <liblary>slide_simple</liblary> \n")
+    saveFileWriteLine("config.xml","    <ttskey>" + ttskey + "</ttskey> \n")
     saveFileWriteLine("config.xml","</summary> \n")
     saveFileWriteLine("config.xml","</class>")
     
