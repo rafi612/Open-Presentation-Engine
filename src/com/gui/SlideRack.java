350bb8b341eadb753575fe334f4974da49473a60
rafi612 <rafi612lol@gmail.com>
2021-06-02 21:38:25 +0200

    Added Slide Rack buttons

--- src/com/gui/SlideRack.java
+++ src/com/gui/SlideRack.java
@@ -1,14 +1,41 @@
 package com.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class SlideRack extends JPanel 
 {
 	private static final long serialVersionUID = 1L;
+	
+	public ArrayList<JButton> actionbuttons;
 
 	public SlideRack() 
 	{
+		setLayout(new BorderLayout());
 		
+        JPanel actions = new JPanel();
+        actions.setLayout(new BoxLayout(actions, 1));
+        actions.setBorder(BorderFactory.createTitledBorder("Actions"));
+        actions.setToolTipText("Actions in Slide Rack");
+        actions.setPreferredSize(new Dimension(200, 0));
+        
+		actionbuttons = new ArrayList<JButton>();
+		actionbuttons.add(new JButton("Add Slide"));
+		actionbuttons.add(new JButton("Delete Slide"));
+		actionbuttons.add(new JButton("Move Up"));
+		actionbuttons.add(new JButton("Move Down"));
+		actionbuttons.add(new JButton("Select All Slides"));
+		
+	    for (int i = 0;i < actionbuttons.size(); i++)
+	    	actions.add(actionbuttons.get(i));
+	        
+	    add(actions,BorderLayout.EAST);
 	}
 
 }
