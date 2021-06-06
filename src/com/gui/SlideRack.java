596aa70eb227bccd2c921f07d932ef4c6e93a1ad
rafi612 <rafi612lol@gmail.com>
2021-06-06 20:05:22 +0200

    Added select all checkbox in slide rack

--- src/com/gui/SlideRack.java
+++ src/com/gui/SlideRack.java
@@ -2,11 +2,13 @@ package com.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 public class SlideRack extends JPanel 
@@ -14,6 +16,8 @@ public class SlideRack extends JPanel
 	private static final long serialVersionUID = 1L;
 	
 	public ArrayList<JButton> actionbuttons;
+	
+	public JCheckBox selected;
 
 	public SlideRack() 
 	{
@@ -31,6 +35,14 @@ public class SlideRack extends JPanel
 		actionbuttons.add(new JButton("Move Up"));
 		actionbuttons.add(new JButton("Move Down"));
 		actionbuttons.add(new JButton("Select All Slides"));
+		
+		JPanel toppanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		toppanel.setBorder(BorderFactory.createTitledBorder(""));
+		
+		selected = new JCheckBox("Select all");
+		toppanel.add(selected);
+		
+		add(toppanel,BorderLayout.NORTH);
 		
 	    for (int i = 0;i < actionbuttons.size(); i++)
 	    	actions.add(actionbuttons.get(i));
