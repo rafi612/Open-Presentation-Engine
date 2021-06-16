a983b3be88828ea67c2f0f523f9d321e625f82aa
rafi612 <rafi612lol@gmail.com>
2021-06-16 18:04:28 +0200

    Added rack element

---
+++ src/com/gui/sliderack/RackElement.java
@@ -0,0 +1,14 @@
+package com.gui.sliderack;
+
+import javax.swing.JPanel;
+
+public class RackElement extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+
+	public RackElement() 
+	{
+		
+	}
+
+}
